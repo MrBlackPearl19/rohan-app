58fc7351fece7cd743826a30f43c8d0a9d2ba89f
MrBlackPearl19 <mr.blackpearl@outlook.com>
2024-08-05 13:35:29 +0530

    new update
    just updated 05-08-24

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+
+// components
+import Navbar from "./components/Working/Navbar";
+import Cover from "./components/Working/Cover";
+import ProductCard from "./components/Working/ProductCard";
+import CustomizedProduct from "./components/Working/CustomizedProduct";
+import RelativeProduct from "./components/Working/RelativeProduct";
+import Deals from "./components/Working/Deals";
+import RPQ from "./components/Working/RPQ";
+import JustForYou from "./components/Working/JustForYou";
+import TradeService from "./components/Working/TradeService";
+import Countries from "./components/Working/Countries";
+import Footer from "./components/Working/Footer";
+
+// data
+import items from "./json/practice/dataItem.json";
+
+import SortingValue from "./components/Working/SortingValue";
 
 function App() {
+  let [allItem, setAllItem] = useState(items.dataItem);
+  let [sorValue, setSortValue] = useState([]);
+  let [flag, setFlag] = useState(true);
+
+  const sortItem = (e) => {
+    allItem.forEach((value, index) => {
+      if (value.categories === e) {
+        setSortValue((val) => [...val, value]);
+      }
+    });
+    setFlag(false);
+  };
+
+  const deleteItem = (e) => {
+    setSortValue((value) => {
+      return value.filter((val) => {
+        if (val.categories.toLowerCase() !== e.toLowerCase()) {
+          return val;
+        }
+      });
+    });
+
+    if (sorValue.length === 4) {
+      setFlag(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <Navbar sortItem={sortItem} deleteItem={deleteItem} />
+      {flag ? null : <SortingValue sortValue={sorValue} />}
+
+      {flag ? <Cover /> : null}
+      {flag ? <ProductCard /> : null}
+      {flag ? <CustomizedProduct /> : null}
+      {flag ? <RelativeProduct /> : null}
+      {flag ? <Deals /> : null}
+      {flag ? <RPQ /> : null}
+      {flag ? <JustForYou /> : null}
+      {flag ? <TradeService /> : null}
+      {flag ? <Countries /> : null}
+
+      <Footer />
+    </React.StrictMode>
   );
 }
 
